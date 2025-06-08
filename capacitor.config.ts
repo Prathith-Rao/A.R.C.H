b0af23396e74cd889a507bed2bd6cf59ea7f4515
Prathith Rao <prathith05@gmail.com>
2025-06-08 21:32:46 +0530

    cap

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.c8e0e97144f74815a48f9d8eda016918',
-  appName: 'echoes-india-voyager',
+  appName: 'A.R.C.Hr',
   webDir: 'dist',
   server: {
     url: "https://c8e0e971-44f7-4815-a48f-9d8eda016918.lovableproject.com?forceHideBadge=true",
